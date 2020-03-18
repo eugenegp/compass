a40d77f3babc191823ea7eab05930a025421a1c5
eugenegp <evgeniy@goldenplanet.com>
2020-03-18 15:55:49 +0200

    fixed azimut detect

--- app/src/main/java/com/eugenekrabs/windcourse/AzimutToImageConverter.java
+++ app/src/main/java/com/eugenekrabs/windcourse/AzimutToImageConverter.java
@@ -5,26 +5,27 @@ import android.util.Log;
 public class AzimutToImageConverter {
 
     public String format(float azimuth, float diff) {
-        Log.d("L", "az: " + azimuth + " | diff: " + diff);
+//        Log.d("L", "az: " + azimuth + " | diff: " + diff);
 
-        azimuth = (azimuth - diff);
-
+//        azimuth = (azimuth - diff);
+        int diffInt = (int)Math.floor((diff + 5) / 10) * 10;
 
         int rounded = (int)Math.floor((azimuth + 5) / 10) * 10;
+
+//        if (rounded > 360) {
+//            rounded = rounded - 360;
+//        }
+
+//        Log.d("L", azimuth + " - " + diffInt + " r: " + rounded + " " + (360 - rounded));
+
         rounded  = 360 - rounded;
-
-        if (rounded > 360) {
-            rounded = rounded - 360;
-        }
-
-        Log.d("L", "az-calc: " + rounded);
 
 
         if (rounded < 0 ) {
             rounded = Math.abs(rounded);
         }
 
-        Log.d("L", "final: " + rounded);
+//        Log.d("L", "final: " + rounded);
 
         if (rounded == 0 || rounded == 360) {
             return "i0";
